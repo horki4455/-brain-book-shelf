db2c63b7245470968f378ea4cd0221298d64333c
hiroki <20131476@kanda.kifl.ac.jp>
2021-09-06 14:01:55 +0900

    [WIP]feat: テーブル追加

---
+++ pages/book/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>{{ count }}</div>
+    <div>あああ</div>
+    <div>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="5"
+        class="elevation-1"
+      />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { ref } from '@vue/composition-api'
+export default {
+  setup() {
+    const count = ref(1)
+    const headers = [
+      {
+        text: 'Dessert (100g serving)',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Calories', value: 'calories' },
+      { text: 'Fat (g)', value: 'fat' },
+      { text: 'Carbs (g)', value: 'carbs' },
+      { text: 'Protein (g)', value: 'protein' },
+      { text: 'Iron (%)', value: 'iron' },
+    ]
+
+    return { count }
+  },
+}
+</script>
+<style></style>
