57af602aa601f4d97742b589b67fdb0e77f1622e
hiroki <20131476@kanda.kifl.ac.jp>
2021-09-07 14:23:28 +0900

    ADD: テーブル・検索機能

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,36 @@
 <template>
-  <v-app>
-    <!-- TODO: d-flex直す -->
-    <v-app-bar color="#4169E1" class="d-flex" dark app height="60">
-      <div>Brain Boolshelf Maker</div>
-      <div>本</div>
-      <div class="btn text-light">マイページの名前表示</div>
-    </v-app-bar>
-  </v-app>
+  <div>
+    <v-app class="app">
+      <v-app-bar color="primary" app dark hight="60">
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-menu offset-y>
+            <template v-slot:activator="{on}">
+              <v-btn v-on="on" text>user name</v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Consulting and support</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Discord community</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar-items>
+      </v-app-bar>
+    </v-app>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </div>
 </template>
 <style scoped>
 .v-application {
---
+++ pages/books/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>{{ count }}</div>
+    <div>詳細</div>
+  </div>
+</template>
+<script lang="ts">
+import { ref } from '@vue/composition-api'
+export default {
+  setup() {
+    const count = ref(1)
+
+    return { count }
+  },
+}
+</script>
+<style></style>
